4ce8676a529d93f87964364be0787ae6c224ed88
renato <renato@twobo.com>
2017-11-04 22:30:48 +0100

    Created Kotlin sample.

---
+++ samples/kotlin/src/hello-world.kt
@@ -0,0 +1,22 @@
+import com.athaydes.osgi.rsa.provider.protobuf.api.RemoteServices
+import java.util.function.IntToDoubleFunction
+
+/**
+ * Starts a client and a server, runs a remote method, then stops the server.
+ */
+fun main(args: Array<String>) {
+    // service implementation
+    val squareCalculator = IntToDoubleFunction { n -> Math.pow(n.toDouble(), 2.0) }
+
+    // create server
+    val server = RemoteServices.provideService(squareCalculator, 8023, IntToDoubleFunction::class.java)
+
+    // create client
+    val client = RemoteServices.createClient(IntToDoubleFunction::class.java, "localhost", 8023)
+
+    server.use { _ ->
+        // invoke the remote service
+        val response = client.applyAsDouble(5)
+        println("The square of 5 is $response")
+    }
+}
